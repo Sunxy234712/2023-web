c2243d3100c7682a025f8df1721c4866cae33844
xiayu <xiayu123>
2024-12-12 14:17:36 +0800

    删除多余文件夹

--- node/projectByExpress/myApp/db/models/userModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const userSchema = new mongoose.Schema({
-    username: String,
-    pwd: String
-})
-const userModel = mongoose.model('userModel', userSchema)
-module.exports = userModel
